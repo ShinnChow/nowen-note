fa20e06ee84ee457bbd7bb52512b2a200a700f07
cropflre <767308865@qq.com>
2026-06-09 09:38:12 +0800

    fix: 优化Android WebView选择菜单处理，使用委托模式替代直接返回null

--- frontend/android/app/src/main/java/com/nowen/note/NoSelectionActionModeWebView.java
+++ frontend/android/app/src/main/java/com/nowen/note/NoSelectionActionModeWebView.java
@@ -3,6 +3,8 @@ package com.nowen.note;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import com.getcapacitor.CapacitorWebView;
 
 public class NoSelectionActionModeWebView extends CapacitorWebView {
@@ -12,11 +14,45 @@ public class NoSelectionActionModeWebView extends CapacitorWebView {
 
     @Override
     public ActionMode startActionMode(ActionMode.Callback callback) {
-        return null;
+        return super.startActionMode(new NoSelectionMenuCallback(callback));
     }
 
     @Override
     public ActionMode startActionMode(ActionMode.Callback callback, int type) {
-        return null;
+        return super.startActionMode(new NoSelectionMenuCallback(callback), type);
+    }
+
+    private static class NoSelectionMenuCallback implements ActionMode.Callback {
+        private final ActionMode.Callback delegate;
+
+        NoSelectionMenuCallback(ActionMode.Callback delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            boolean created = delegate == null || delegate.onCreateActionMode(mode, menu);
+            menu.clear();
+            return created;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            boolean prepared = delegate != null && delegate.onPrepareActionMode(mode, menu);
+            menu.clear();
+            return prepared;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return true;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            if (delegate != null) {
+                delegate.onDestroyActionMode(mode);
+            }
+        }
     }
 }
